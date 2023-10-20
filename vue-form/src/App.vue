5d55d30759de5a0269b1b4cfb912d5e1deef3062
bumsoo <chorocsky@gmail.com>
2023-10-20 14:50:48 +0900

    사용자 입력 폼 - submit 이벤트 처리

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <form action="">
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id : </label>
-      <input id="username" type="text">
+      <input id="username" type="text" v-model="username">
     </div>
     <div>
       <label for="password">pw : </label>
-      <input id="password" type="password">
+      <input id="password" type="password" v-model="password">
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function(event) {
+      //submit 태그 값을 전달하면 페이지 새로고침을 막는 메서드
+      event.preventDefault();
+      
+      console.log(this.username, this.password);
+    }
+  }
 }
 </script>
 
