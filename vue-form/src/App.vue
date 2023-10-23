c50889e92141110bcd80bfb0c45d6177aac3ec22
bumsoo <chorocsky@gmail.com>
2023-10-23 13:40:30 +0900

    사용자 입력 폼 - axios

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <form v-on:submit="submitForm">
+  <!-- 폼의 기본적인 동작을 막기 위한 modifier로써 흔히 사용하는 event.preventDefault()와 같은 효과 -->
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id : </label>
       <input id="username" type="text" v-model="username">
@@ -13,6 +14,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function() {
     return {
@@ -21,11 +24,25 @@ export default {
     }
   },
   methods: {
-    submitForm: function(event) {
+    submitForm: function() {
       //submit 태그 값을 전달하면 페이지 새로고침을 막는 메서드
-      event.preventDefault();
+      // event.preventDefault();
       
       console.log(this.username, this.password);
+
+      var url = 'https://jsonplaceholder.typicode.com/users/';
+      var data = {
+        username: this.username,
+        password: this.password
+      };
+      
+      axios.post(url, data)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
     }
   }
 }
